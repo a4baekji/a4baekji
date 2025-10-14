ef0b4bd4f9ed66aec7b724fe588a92556c5a4070
a4baekji <152164827+a4baekji@users.noreply.github.com>
2025-10-14 17:24:13 +0900

    [Bronze V] Title: 사분면 고르기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,13 @@
+num1 = int(input())
+num2 = int(input())
+
+if num1 > 0:
+    if num2 > 0:
+        print(1)
+    else:
+        print(4)
+else:
+    if num2 > 0:
+        print(2)
+    else:
+        print(3)
