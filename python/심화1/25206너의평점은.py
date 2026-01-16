5959aa94f1a80078b00c16e0c468c970b07acc56
a4baekji <152164827+a4baekji@users.noreply.github.com>
2026-01-16 07:59:57 +0000

    백준 단계별 문제풀이 심화1 25206 너의 평점은 문제풀이

---
+++ python/심화1/25206너의평점은.py
@@ -0,0 +1,17 @@
+import sys
+score = {'A+': 4.5, 'A0' : 4.0, 
+         'B+': 3.5, 'B0': 3.0, 
+         'C+': 2.5, 'C0': 2.0, 
+         'D+': 1.5, 'D0': 1.0, 
+         'F': 0.0}
+total = 0.0
+s = 0.0
+
+for _ in range(20):
+    subject, a, b = input().split()
+    if b in score:
+        total += float(a) * score[b]
+        s += float(a)
+
+print(total / s)
+
