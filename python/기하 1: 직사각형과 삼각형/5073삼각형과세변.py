f27040c2851247e9eaa3b2db0cf74ebf09143872
a4baekji <152164827+a4baekji@users.noreply.github.com>
2026-03-08 06:33:23 +0000

    백준 단계별문제풀이 기하1 5073 삼각형과 세 변

---
+++ python/기하 1: 직사각형과 삼각형/5073삼각형과세변.py
@@ -0,0 +1,12 @@
+while(1):
+    a, b, c = map(int, input().split())
+    if(a == b == c == 0):
+        break
+    elif(max(a, b, c) >= a+b+c - max(a,b,c)):
+        print("Invalid")
+    elif(a == b== c):
+        print("Equilateral")
+    elif(a == b or b == c or c == a):
+        print("Isosceles")
+    else:
+        print("Scalene")
